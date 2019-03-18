88ba68b615d62dec63dcfabb1923c62e3c19a9fe
sultan <aidaraliuulu@gmail.com>
2019-03-18 15:14:46 +0600

    req.status(400)

--- src/messages.js
+++ src/messages.js
@@ -6,17 +6,13 @@ router.get('/', (req , res) => {
    res.send(fileDb.getItems())
 });
 
-router.get('/:data', (req , res) => {
-    res.send(fileDb.getItems())
-});
-
 router.post('/', (req, res) => {
     console.log(req.body);
     if (req.body.author !== "" && req.body.message !== "") {
         fileDb.addItem(req.body);
         res.send({message: "OK"})
     } else {
-        res.send({message: 'Нет автора или собщения'})
+        res.status(400).send({message: 'Нет автора или собщения'})
     }
 
 });
